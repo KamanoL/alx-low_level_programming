7543b646f392b1782b757994a712fad1cf3ae19e
KamanoL <tiisangl7@gmail.com>
2023-07-13 10:15:29 -0700

    Betty 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <unstd.h>
+#include <unistd.h>
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-write(2, "and peace of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+write(2, "and piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
