7dbb57ae89c2ace57b27bfa46651a88b4efe29db
KamanoL <tiisangl7@gmail.com>
2023-07-13 03:40:03 -0700

    Puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A C program to print using puts and is followed by A new line
+ *
+ * Return: Always 0(Success)
+ *
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+Return(0);
+}
