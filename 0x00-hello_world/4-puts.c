2a1bb5d9452d1fdf5bf1a83d9f7d5b8c2abf5395
KamanoL <tiisangl7@gmail.com>
2023-07-13 06:56:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- * main - Entry point
- *
- * Return: ALways 0 (Success)
+ *main - Entry point
+ *Return: ALways 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like buiding a multiligual puzzle");
-	return (0);
+puts("\"Programming is like buiding a multiligual puzzle");
+return (0);
 }
