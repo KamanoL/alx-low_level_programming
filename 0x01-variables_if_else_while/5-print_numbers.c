2f43ca07aea0ffd0ab525655d11785539b61c4a6
KamanoL <tiisangl7@gmail.com>
2023-07-14 13:03:50 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Printing nubers in single base
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		printf("%d", l);
+	}
+	printf("\n");
+	return (0);
+}
