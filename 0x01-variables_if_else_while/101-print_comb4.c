1d5fcd9b2190f70d5d468a30470ed068487f6cce
KamanoL <tiisangl7@gmail.com>
2023-07-14 17:36:33 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - posible combination of numbers
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int t;
+	int c;
+	int l;
+
+	for (t = 48; t < 58; t++)
+	{
+		for (c = 48; c < 58; c++)
+		{
+			if (c > t)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					if (l > t)
+					{
+					putchar(t);
+					putchar(c);
+					putchar(l);
+					if (t == 55 && c == 56 && l == 57)
+					{
+						break;
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
