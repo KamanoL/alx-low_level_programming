156c466ed5b34ea1b7b64b0dc449efed817cafdd
KamanoL <tiisangl7@gmail.com>
2023-07-18 08:11:40 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <main.h>
+/**
+ * main - prints _putchar
+ * writen by less
+ * Return: Alwaysb 0
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("ptchar");
+		_putchar('\n');
+	return (0);
+}
