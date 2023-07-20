007789bc278a0f3b176fa50d1b33be165e5e0538
KamanoL <tiisangl7@gmail.com>
2023-07-20 16:08:05 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - prints charecters and or digits like (0 to 9).
+ * Return: 1 always set
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar('a');
+	_putchar('\n');
+	}
+	return (0);
+}
