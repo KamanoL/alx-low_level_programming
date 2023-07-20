f8acb83cb26fe48ac8c973ed4453c96115a694a5
KamanoL <tiisangl7@gmail.com>
2023-07-20 15:31:48 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies numbers or digits
+ * @b: b - variable
+ * @a: a - variable
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	while (a * b = c)
+	{
+		_putchar(0);
+	}
+}
