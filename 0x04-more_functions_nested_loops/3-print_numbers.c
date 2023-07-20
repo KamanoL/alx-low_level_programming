7ebdb6a1cc1f6d7a3090750749d0816f29dc09dc
KamanoL <tiisangl7@gmail.com>
2023-07-20 16:47:39 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,14 @@
 
 /**
  * print_numbers - prints charecters and or digits like (0 to 9).
+ *
  * Return: 0 always set
  */
-
 void print_numbers(void)
 {
 	int a;
 
-	for (a = 48; a <= 58; a++)
+	for (a = 48; a < 58; a++)
 	{
 		_putchar(a);
 	}
