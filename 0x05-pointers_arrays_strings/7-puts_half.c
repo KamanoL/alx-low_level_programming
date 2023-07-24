683060eee3e7252fa52004c372eafdb6346de9f7
KamanoL <tiisangl7@gmail.com>
2023-07-24 13:15:14 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,9 @@ void puts_half(char *str)
 	int count = 0;
 
 	for (l = 0; str[l] != '\0'; l++)
-	{
 		count++;
 	n = (count - 1) / 2;
-	}
 	for (l = n + 1; str[l] != '\0'; l++)
-	{
 		_putchar(str[l]);
-	}
 	_putchar('\n');
 }
