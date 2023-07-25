f9b49b7ebea07e7eba19b9c3cafa9e78ec78485d
KamanoL <tiisangl7@gmail.com>
2023-07-25 12:49:20 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a function to a string
+ * @s: the pointer to convert
+ * Return: an integer
+ */
+
+int _atoi(char *s)
+{
+	int t, k, l, len, n, digit;
+	t = 0;
+	k = 0;
+	l = 0;
+	len = 0;
+	n = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (t < len && n == 0)
+	{
+		if (s[t] == 45)
+		{
+
+		}
+	}
+}
