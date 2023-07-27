2c1fbae7b50f3f4696e562f6bf7733ddf5408102
KamanoL <tiisangl7@gmail.com>
2023-07-27 09:57:21 -0700

    rot

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string
+ * @n: pointer to string
+ * @data1: result
+ * @datarot: result
+ * Return: *n
+ */
+
+char *rot13(char *n)
+{
+	int l, k;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (l = 0; n[l] != '\0'; l++)
+	{
+		for (k = 0; k < 52; k++)
+		{
+			if (n[l] == datarot[k])
+			{
+				n[l] = data1[k];
+				break;
+			}
+		}
+	}
+	return (n);
+}
