f44c7b0d660e5ed3ff21d2e1adc85c2634054b41
KamanoL <tiisangl7@gmail.com>
2023-08-17 07:09:59 -0700

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,8 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg>
+
+/**
+ * print_strings - prints strings and new line
+ * @separator: string to print
+ * @n: number of strings passed
