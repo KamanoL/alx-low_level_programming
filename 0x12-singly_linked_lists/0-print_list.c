87948c90116c68cd4f08fcca0a52ba15ffa51c9f
KamanoL <tiisangl7@gmail.com>
2023-08-24 10:13:12 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,6 +4,7 @@
 /**
  * print_list - prints all elements of linked list
  * @h: pointer to list_t list to print
+ *
  * Return: number of nodes printed
  */
 
@@ -20,5 +21,6 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		s++;
 	}
+
 	return (s);
 }
