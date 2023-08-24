3013122892af7b514dc86b128b3cca4bccb2c861
KamanoL <tiisangl7@gmail.com>
2023-08-24 07:16:33 -0700

    first files

---
+++ project.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * putcharecter - prints charecters
+ * @c: charecter to print
+ * Return: 0 (Success) else 98
+ */
+
+void putcharecter(char *)
+{
+	printf("%c\n", char *);
+}
+
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char _char[];
+	int T;
+
+	if (T == _char[])
+		_char[T];
+	{
+		putcharecter("_char");
+	}
+	return (0);
+}
