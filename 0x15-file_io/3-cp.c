bbaba3d5facdaf77bcbd3a5a2971aae075bdc274
tiisang <tiisangl7@gmail.com>
2023-10-02 13:57:30 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,100 +1,60 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
 
-char *creat_buff(char *file);
-void end_file(int fd);
-
-/**
- * creat_buff - allocates bytes
- * @file: name of file
- *
- * Return: allocated buffer pointer
- */
-
-char *creat_buff(char *file)
-{
-	char *buff;
-
-	buff = malloc(sizeof(char) * 1024);
-
-	if (buff == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
-		exit(97);
-	}
-
-	return (buff);
-}
+#ifndef BUFF_SIZE
+#define BUFF_SIZE 1024
+#endif
 
 /**
- * end_file - ends file
- * @fd: file description
- *
- * Return: None
- */
-
-void end_file(int fd)
-{
-	int a;
-
-	a = close(fd);
-
-	if (a == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close dd %d\n", fd);
-		exit(98);
-	}
-}
-
-/**
- * main - duplicates content of a fileto the next
- * @argc: argument count
- * @argv: argument vector
- * Return:0 Always (Success) else NULL
+ * main - duplicates a file to the next
+ * @argc: file name
+ * @argv: number of the letters used
+ * Return: Always 0 (Success), else NULL
  */
 
 int main(int argc, char *argv[])
 {
-	int to, from, a, t;
-	char *buff;
+    int origin, destination;
+    ssize_t num;
+    char buf[BUFF_SIZE];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from_to\n");
-		exit(97);
-	}
-
-	buff = creat_buff(argv[1]);
-	from = open(argv[1], O_RDONLY);
-	a = read(from, buff, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (from == -1 || a == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buff);
-			exit(98);
-		}
-
-		t = write(to, buff, a);
-		if (to == -1 || t == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", argv[2]);
-			free(buff);
-			exit(99);
-		}
-
-		a = read(from, buff, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (a > 0);
-
-	free(buff);
-	end_file(from);
-	end_file(to);
-
-	return (0);
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+    origin = open(argv[1], O_RDONLY);
+    if (origin == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    destination = open(argv[2], O_CREAT | O_WRONLY, 0664);
+    if (destination == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
+    }
+    while ((num = read(origin, buf, BUFF_SIZE)) > 0)
+    {
+        if (write(destination, buf, num) != num)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+    }
+    if (num == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+    if ((close(origin) == -1) || (close(destination) == -1))
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", origin);
+        exit(100);
+    }
+    exit(EXIT_SUCCESS);
 }
