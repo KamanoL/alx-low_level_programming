669739225590b20a9a5af5dd7dc274dbf165d778
Kamanol <tiisangl7@gmail.com>
2023-12-01 05:10:54 -0800

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - adds texts to a file
+ * @filename: name the file to be used
+ * @text_content: string
+ *
+ * Return: if NULL -1 else success 1
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+	int a, l, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (filename != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	a = open(filename, O_WRONLY | O_APPEND);
+	l = write(a, text_content, length);
+
+	if (a == -1 || l == -1)
+		return (-1);
+
+	close(a);
+	return (1);
+}
