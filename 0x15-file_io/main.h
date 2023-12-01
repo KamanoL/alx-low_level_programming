fd213122fa7a2299a7b9c27de2386504c3adf672
Kamanol <tiisangl7@gmail.com>
2023-12-01 04:59:07 -0800

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define POXI_C_SOURCE 200809L
+#define READ_BYTES 1204
+#define BUFF_SIZE
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
